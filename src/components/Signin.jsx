eac1913ad2f1c69b6d01414237ca76499cf54356
Alin Eduard Iacoban <aliniacoban@yahoo.com>
2022-12-07 15:05:19 +0200

    Styling using Tailwind for Login+Register form

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
+import authImg from "../assets/authImage.jpg";
 
 const Signin = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  // eslint-disable-next-line no-unused-vars
   const [error, setError] = useState("");
   const navigate = useNavigate();
   const { signIn } = UserAuth();
@@ -22,37 +24,43 @@ const Signin = () => {
   };
 
   return (
-    <div className="max-w-[700px] mx-auto my-16 p-4">
-      <div>
-        <h1 className="text-2xl font-bold py-2">Sign in to your account</h1>
-        <p className="py-2">
-          Don't have an account yet?{" "}
-          <Link to="/signup" className="underline">
-            Sign up.
-          </Link>
-        </p>
+    <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+      <div className="hidden sm:block">
+        <img className="w-full h-full object-cover" src={authImg} alt="" />
       </div>
-      <form onSubmit={handleSubmit}>
-        <div className="flex flex-col py-2">
-          <label className="py-2 font-medium">Email Address</label>
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            className="border p-3"
-            type="email"
-          />
+
+      <div className="max-w-[700px] mx-auto my-16 p-4">
+        <div>
+          <h1 className="text-2xl font-bold py-2">Sign in to your account</h1>
         </div>
-        <div className="flex flex-col py-2">
-          <label className="py-2 font-medium">Password</label>
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            className="border p-3"
-            type="password"
-          />
-        </div>
-        <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white">
-          Sign In
-        </button>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col text-gray-400 py-2">
+            <label className="py-2 font-medium">Email Address</label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+              type="email"
+            />
+          </div>
+          <div className="flex flex-col text-gray-400 py-2">
+            <label className="py-2 font-medium">Password</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              className="p-2 rounded-lg bg-gray-700 mt-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+              type="password"
+            />
+          </div>
+          <button className="w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg">
+            Sign In
+          </button>
+          <p className="py-2">
+            Don't have an account yet?{" "}
+            <Link to="/signup" className="underline">
+              Sign up.
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,10 +2,12 @@ import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { UserAuth } from "../context/AuthContext";
 import "../index.css";
+import authImg from "../assets/authImage.jpg";
 
 const Signup = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  // eslint-disable-next-line no-unused-vars
   const [error, setError] = useState("");
   const { createUser } = UserAuth();
   const navigate = useNavigate();
@@ -23,37 +25,42 @@ const Signup = () => {
   };
 
   return (
-    <div className="max-w-[700px] mx-auto my-16 p-4">
-      <div>
-        <h1 className="text-2xl font-bold py-2">Sign up for a free account</h1>
-        <p className="py-2">
-          Already have an account yet?{" "}
-          <Link to="/" className="underline">
-            Sign in.
-          </Link>
-        </p>
+    <div className="grid grid-cols-1 sm:grid-cols-2 h-screen w-full">
+      <div className="hidden sm:block">
+        <img className="w-full h-full object-cover" src={authImg} alt="" />
       </div>
-      <form onSubmit={handleSubmit}>
-        <div className="flex flex-col py-2">
-          <label className="py-2 font-medium">Email Address</label>
-          <input
-            onChange={(e) => setEmail(e.target.value)}
-            className="border p-3"
-            type="email"
-          />
+      <div className="max-w-[700px] mx-auto my-16 p-4">
+        <div>
+          <h1 className="text-2xl font-bold py-2">Sign up for free</h1>
         </div>
-        <div className="flex flex-col py-2">
-          <label className="py-2 font-medium">Password</label>
-          <input
-            onChange={(e) => setPassword(e.target.value)}
-            className="border p-3"
-            type="password"
-          />
-        </div>
-        <button className="border border-blue-500 bg-blue-600 hover:bg-blue-500 w-full p-4 my-2 text-white">
-          Sign Up
-        </button>
-      </form>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col text-gray-400 py-2">
+            <label className="py-2 font-medium">Email Address</label>
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              className="rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+              type="email"
+            />
+          </div>
+          <div className="flex flex-col text-gray-400 py-2">
+            <label className="py-2 font-medium">Password</label>
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              className="p-2 rounded-lg bg-gray-700 mt-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none"
+              type="password"
+            />
+          </div>
+          <button className="w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg">
+            Sign Up
+          </button>
+          <p className="py-2">
+            Already have an account ?{" "}
+            <Link to="/" className="underline">
+              Sign in.
+            </Link>
+          </p>
+        </form>
+      </div>
     </div>
   );
 };
