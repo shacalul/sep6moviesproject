5e1ecdbc58568f6d962d90e462962f380515a388
xandrusia <sandragrzelak00@wp.pl>
2022-12-09 02:57:49 +0100

    Fiebase Database creation and push new favourites

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import {getDatabase, push, ref, set} from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -8,6 +9,7 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyC3XIzoowl2wVgwbKOkVivtd0yC-mNdk8w",
   authDomain: "semester6movies.firebaseapp.com",
+  databaseURL: "https://semester6movies-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "semester6movies",
   storageBucket: "semester6movies.appspot.com",
   messagingSenderId: "320084724018",
@@ -18,3 +20,28 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export default app;
+
+//initialize realtime database
+
+export function writeUserData(userId, movieId)
+{
+  const db = getDatabase();
+  try{
+    const usersRef = ref(db,'users/'+userId+'/favourites');
+    const newFavorite=push(usersRef);
+    set(newFavorite ,movieId);
+
+    // var list =  [movieId];
+    // const arrays = ref(db,'arrays/');
+    // const newArr=push(arrays);
+    // set(newArr ,list);
+    
+  }catch(e){
+    console.trace(e);
+    
+  }
+
+
+}
+
+
